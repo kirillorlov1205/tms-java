88aac632cacb1851a29daceaac8d9b99b73528c3
Kiryl Arlou <Kiryl.Arlou@itechart-group.com>
2023-06-30 12:52:04 +0300

    lesson2 task 1 and 2 have been done

---
+++ src/lesson2/Enums.java
@@ -0,0 +1,11 @@
+package lesson2;
+
+public class Enums {
+
+	public enum Seasons{
+		WINTER,
+		SPRING,
+		SUMMER,
+		AUTUMN,
+	}
+}
---
+++ src/lesson2/Main.java
@@ -0,0 +1,12 @@
+package lesson2;
+
+import lesson2.Practise;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Practise.task1();
+		Practise.task2();
+		Practise.task3(3);
+	}
+}
