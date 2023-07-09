e08ca7d367c849f0465338d7b637c240eb925d72
Kiryl Arlou <Kiryl.Arlou@itechart-group.com>
2023-07-09 17:13:44 +0300

    lesson3 college system fixes

---
+++ src/lesson3/collegeSystem/Journal.java
@@ -0,0 +1,106 @@
+package lesson3.collegeSystem;
+
+import lesson3.Utilities;
+
+import java.util.ArrayList;
+
+public class Journal {
+
+    private static ArrayList<Student> journal = new ArrayList<>();
+
+    public static void menu() {
+        int choice = 0;
+        do {
+            System.out.println("Choose option:");
+            System.out.println("1. Get group with lower average mark\n" +
+                    "2. Remove students with lowest mark (<4)\n" +
+                    "3. Show all students\n" +
+                    "4. Show group\n" +
+                    "5. Exit");
+
+            choice = Utilities.getConsoleNumber();
+
+            switch (choice) {
+                case 1:
+                    getGroupWithLowerAverageMark();
+                    break;
+                case 2:
+                    removeStudentsWithLowestMark();
+                    break;
+                case 3:
+                    showAllStudents();
+                    break;
+                case 4:
+                    System.out.println("Select group 1 or 2");
+                    int group = Utilities.getConsoleNumber();
+                    if (group == 1) {
+                        showGroup(Enums.Groups.GROUP_1);
+                    } else if (group == 2) {
+                        showGroup(Enums.Groups.GROUP_2);
+                    } else {
+                        System.out.println();
+                        System.out.println("There is no such group");
+                    }
+                    break;
+                case 5:
+                    System.out.println("Good day!");
+                    break;
+            }
+        } while (choice != 5);
+    }
+
+    private static void showAllStudents() {
+        System.out.println(journal);
+    }
+
+    private static void showGroup(Enums.Groups groupNumber) {
+        ArrayList<Student> group = new ArrayList<>();
+
+        for (Student student : journal) {
+            if (student.getGroup() == groupNumber) {
+                group.add(student);
+            }
+        }
+        System.out.println(group);
+    }
+
+    private static void removeStudentsWithLowestMark() {
+        ArrayList<Student> studentToRemove = new ArrayList<>();
+
+        for (Student student : journal) {
+            if (student.getMark() < 4) {
+                studentToRemove.add(student);
+                System.out.println("Student: '" + student.getName() + "' has been removed");
+            }
+        }
+        journal.removeAll(studentToRemove);
+    }
+
+    private static void getGroupWithLowerAverageMark() {
+        double averageValueOfGroup1 = 0;
+        double averageValueOfGroup2 = 0;
+
+        for (Student student : journal) {
+            if (student.getGroup() == Enums.Groups.GROUP_1) {
+                averageValueOfGroup1 += student.getMark();
+            } else {
+                averageValueOfGroup2 += student.getMark();
+            }
+        }
+
+        System.out.println("Group 1 average mark is: '" + averageValueOfGroup1 + "'\n" +
+                "Group 2 average mark is: '" + averageValueOfGroup2 + "'");
+
+        if (averageValueOfGroup1 > averageValueOfGroup2) {
+            System.out.println("Group 2 has lower average mark than group 1");
+        } else if (averageValueOfGroup1 < averageValueOfGroup2) {
+            System.out.println("Group 1 has lower average mark than group 2");
+        } else {
+            System.out.println("Group 1 and group 2 have equal average marks");
+        }
+    }
+
+    public static ArrayList<Student> getJournal() {
+        return journal;
+    }
+}
--- src/lesson3/collegeSystem/Main.java
+++ src/lesson3/collegeSystem/Main.java
@@ -1,15 +1,18 @@
 package lesson3.collegeSystem;
 
-import java.util.ArrayList;
+import java.util.Arrays;
 
 public class Main {
-	public static void main(String[] args) {
+    public static void main(String[] args) {
 
-		System.journal.add(new Student("Kirill", Enums.Groups.GROUP_1, 10));
-		System.journal.add(new Student("Dany", Enums.Groups.GROUP_1, 2));
-		System.journal.add(new Student("Denis", Enums.Groups.GROUP_2, 3));
-		System.journal.add(new Student("Mark", Enums.Groups.GROUP_2, 10.4));
+        Student[] arr = {new Student("Kirill", Enums.Groups.GROUP_1, 10),
+                new Student("Dany", Enums.Groups.GROUP_1, 2),
+                new Student("Denis", Enums.Groups.GROUP_2, 3),
+                new Student("Mark", Enums.Groups.GROUP_2, 10.4)
+        };
 
-		System.menu();
-	}
+        Journal.getJournal().addAll(Arrays.asList(arr));
+
+        Journal.menu();
+    }
 }
